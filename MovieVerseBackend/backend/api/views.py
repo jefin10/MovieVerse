1ca861f1a259b661fcf57758c34c94fab050cc80
jenjose72 <jenjose72@gmail.com>
2025-05-08 18:01:32 +0530

    initial files

---
+++ MovieVerseBackend/backend/api/views.py
@@ -0,0 +1,11 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def hello(request):
+    return Response({"message": "Hello from Django!"})
+# Add more API view functions here
+
+@api_view(['GET'])
+def get_movies(request):
+    return Response({"movies": ["Movie 1", "Movie 2", "Movie 3"]})
