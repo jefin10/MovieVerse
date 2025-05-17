5c32bc92f2eb49e2165a86c5117f312c10d6c131
Jefin Francis <147376215+jefin10@users.noreply.github.com>
2025-05-17 18:10:46 +0530

    Final donennenenen

--- MovieVerseBackend/backend/api/urls.py
+++ MovieVerseBackend/backend/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from .views import (
    trending_movies, view_watchlist, add_movie_to_watchlist,
-    remove_movie_from_watchlist,rate_movie,get_movie_rating,add_temp_recommendations,get_user_recommendations, tinder_movies, search_movie, view_watchlist, add_movie_to_watchlist, remove_movie_from_watchlist
+    remove_movie_from_watchlist,get_ratings_based_recommendations,rate_movie,get_movie_rating,add_temp_recommendations,get_user_recommendations, tinder_movies, search_movie, view_watchlist, add_movie_to_watchlist, remove_movie_from_watchlist
 )
 
 urlpatterns = [
@@ -27,6 +27,8 @@ urlpatterns = [
     path('movie/<int:movie_id>/rate/', rate_movie, name='rate_movie'),
     path('movie/<int:movie_id>/rating/', get_movie_rating, name='get_movie_rating'),
     path('searchMovie/<str:query>/',views.search_movie, name='search_movie'),
+    path('recommendations/from-ratings/', get_ratings_based_recommendations, name='ratings_based_recommendations'),  # Add this line
+
       # Add this line
 
     #END OF ROUTES
